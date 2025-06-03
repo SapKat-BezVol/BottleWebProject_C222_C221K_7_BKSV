b3b6175a401a3bee3c32edccdf507517073aa565
Semyon Saprykin <78843201+vemneyy@users.noreply.github.com>
2025-06-03 22:35:34 +0300

    Fix variant tabs

--- static/scripts/local/variant4.js
+++ static/scripts/local/variant4.js
@@ -1,5 +1,4 @@
-document.getElementById("saveForm").addEventListener("submit", function (e) {
-    // Копируем значения из основной формы
-    document.getElementById("save_target_col").value = document.getElementById("target_col").value;
-    document.getElementById("save_features").value = document.getElementById("features").value;
+document.getElementById('saveForm').addEventListener('submit', function () {
+    document.getElementById('save_target_col').value = document.getElementById('target_col').value;
+    document.getElementById('save_features').value = document.getElementById('features').value;
 });
